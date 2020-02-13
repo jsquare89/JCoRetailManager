d80c0d99b194125df40e9760c959a92c704d175d
Jarred <jsquare89@users.noreply.github.com>
2020-02-13 15:19:32 -0800

    Replace Container Reference in ShellVM
    Replace contrainer reference with the IoC reference instead

--- JRMDesktopUI/ViewModels/ShellViewModel.cs
+++ JRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -12,16 +12,13 @@ namespace JRMDesktopUI.ViewModels
     {
         private IEventAggregator _events;
         private SalesViewModel _salesVM;
-        private SimpleContainer _container;
-        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM,
-            SimpleContainer containter)
+        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM)
         {
             _events = events;
             _salesVM = salesVM;
-            _container = containter;
 
             _events.Subscribe(this);
-            ActivateItem(_container.GetInstance<LoginViewModel>());
+            ActivateItem(IoC.Get<LoginViewModel>());
         }
 
         public void Handle(LogOnEvent message)
