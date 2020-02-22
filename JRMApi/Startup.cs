6a171742dba32f053a66fceab99a58b9dd707243
Jarred <jsquare89@users.noreply.github.com>
2020-02-22 13:46:16 -0800

    Moved Secure Key to Configuration

--- JRMApi/Startup.cs
+++ JRMApi/Startup.cs
@@ -60,7 +60,7 @@ namespace JRMApi
                     {
 
                         ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MySecretKeyIsSecretSoDoNotTell")),
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetValue<string>("Secrets:SecurityKey"))),
                         ValidateIssuer = false,
                         ValidateAudience = false,
                         ValidateLifetime = true,
